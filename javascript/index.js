8e2a8fb79b614bd5479cc08554b8618f7fcfd601
Heimdrock <81688442+Heimdrock@users.noreply.github.com>
2024-05-02 09:49:43 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,12 @@
 function menuBarOpen (){
   document.getElementById("navBar").style.height="60px";
-  document.getElementById("pokediv").style.padding-bottom="60px";
+  document.getElementById("pokediv").style.paddingBottom="60px";
   document.getElementById("menuOpen").style.display="none";
   document.getElementById("menuClose").style.display="inline";
 }
 function menuBarClose (){
   document.getElementById("navBar").style.height="0px";
-  document.getElementById("pokediv").style.padding-bottom="0px";
+  document.getElementById("pokediv").style.paddingBottom="0px";
   document.getElementById("menuOpen").style.display="inline";
   document.getElementById("menuClose").style.display="none";
 }
