6f309058504e030291a5f5bec187ded18eaea166
Heimdrock <81688442+Heimdrock@users.noreply.github.com>
2024-05-02 06:58:44 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,9 @@
 function css1Function (){
-  document.getElementById("styleLink").href="../a/css/glowy.css"
+  document.getElementById("styleLink").href="../a/css/glowy.css";
 }
 function css2Function (){
-  documnet.getElementById("styleLink").href="../a/css/
+  documnet.getElementById("styleLink").href="../a/css/neoMorph.css";
+}
+function css3Function (){
+  document.getElementById("styleLink").href="";
 }
