76ed24549a0182b6b284773f1a4e224dbcde4b9a
Heimdrock <81688442+Heimdrock@users.noreply.github.com>
2024-05-02 06:27:56 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,6 @@
-
+function css1Function (){
+  document.getElementById("styleLink").href="../a/css/glowy.css"
+}
+function css2Function (){
+  documnet.getElementById("styleLink").href="../a/css/
+}
