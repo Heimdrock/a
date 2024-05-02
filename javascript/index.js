b51696005d7a7b4ff32b8963c8faedb7f7ae76f6
Heimdrock <81688442+Heimdrock@users.noreply.github.com>
2024-05-02 08:44:13 -0400

    navbar functionality index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,22 @@
+function menuBarOpen (){
+  document.getElementById("navBar").style.display="flex";
+  document.getElementById("navBar").style.height="auto";
+  document.getElementById("menuOpen").style.display="none";
+  document.getElementById("menuClose").style.display="inline";
+}
+function menuBarClose (){
+  document.getElementById("navBar").style.display="none";
+  document.getElementById("navBar").style.height="0";
+  document.getElementById("menuOpen").style.display="inline";
+  document.getElementById("menuClose").style.display="none";
+}
+
 function css1Function (){
   document.getElementById("styleLink").href="../a/css/glowy.css";
 }
 function css2Function (){
-  documnet.getElementById("styleLink").href="../a/css/neoMorph.css";
+  documnet.getElementById("styleLink").href="../a/css/glowy.css";
 }
 function css3Function (){
-  document.getElementById("styleLink").href="";
+  document.getElementById("styleLink").href="../a/css/glowy.css";
 }
